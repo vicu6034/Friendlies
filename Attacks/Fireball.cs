7b92f2a367191465071b38ff76f002379f82295f
Vince Curran <curranvince@yahoo.com>
2021-05-13 00:11:23 -0600

    slight fireball progress

--- Attacks/Fireball.cs
+++ Attacks/Fireball.cs
@@ -9,7 +9,8 @@ namespace Friendlies.Attacks
         public static GameObject Get(
             GameObject owner,
             string weaponName = "BowHuntsman",
-            string projectileName = "bow_projectile_fire"
+            //string animationName = "spear_throw"
+            string projectileName = "GoblinShaman_projectile_fireball"
             )
         {
             bool alreadyExisted = false;
@@ -28,56 +29,40 @@ namespace Friendlies.Attacks
             shared.m_name = "Fireball Bow";
             shared.m_description = "A bow that shoots fireballs";
             shared.m_useDurability = false;
-            shared.m_ammoType = "";
+            shared.m_dodgeable = true;
+            shared.m_blockable = true;
             shared.m_attackForce = 2f;
-            
-            shared.m_damages.m_fire = 30f;
-
-            shared.m_aiAttackRange = 50f;
-            shared.m_aiAttackRangeMin = 16f;
-            shared.m_aiAttackInterval = 40f;
-            //shared.m_aiAttackInterval = 8f;
-            shared.m_aiAttackMaxAngle = 13f;
+            shared.m_ammoType = "";
             /*
-            shared.m_attack.m_projectileVel = 20f;
-
-            GameObject ulty = RRRLateLoadPrefabs.Clone(projectileName, "newFire", true, true);
+            shared.m_attack.m_attackType = Attack.AttackType.Projectile;
+            shared.m_itemType = ItemDrop.ItemData.ItemType.Bow;
+            shared.m_animationState = ItemDrop.ItemData.AnimationState.Bow;
+            */
+            shared.m_skillType = Skills.SkillType.FireMagic;
+            
+            //shared.m_aiAttackRange = 40f;
+            shared.m_aiAttackRangeMin = 0f;
+            shared.m_aiAttackInterval = 4f;
+            //shared.m_holdAnimationState = "";
+            shared.m_damages.m_fire = 30f;
+            //shared.m_attack.m_attackAnimation = "unarmed_punch0";
+            GameObject ulty = RRRLateLoadPrefabs.Clone("bow_projectile", "newFire", true, true);
+            
             Projectile projectile = ulty.GetComponent<Projectile>();
             projectile.m_gravity = 0f;
             projectile.m_aoe = 1.5f;
-            projectile.m_hitEffects.m_effectPrefabs[0].m_scale = true;
-            projectile.m_hitEffects.m_effectPrefabs[1].m_scale = true;
-            */
-            /*
+            
             for (int index = 0; index < ulty.transform.childCount; ++index)
             {
                 GameObject.Destroy(ulty.transform.GetChild(index).gameObject);
             }
-            GameObject prefab = ZNetScene.instance.GetPrefab("Imp_fireball_projectile");
+            GameObject prefab = ZNetScene.instance.GetPrefab(projectileName);
             for (int index = 0; index < prefab.transform.childCount; ++index)
             {
                 GameObject gameObject = UnityEngine.Object.Instantiate<GameObject>(prefab.transform.GetChild(index).gameObject, ulty.transform);
                 gameObject.name = gameObject.name.TrimCloneTag();
             }
-            */
-            /*
-            GameObject boom = RRRLateLoadPrefabs.Clone("sfx_ice_destroyed", "newIce", true, true);
-            ZSFX zSFX = boom.GetComponent<ZSFX>();
-            zSFX.m_minVol = 20f;
-            zSFX.m_maxVol = 25f;
-            zSFX.m_minPitch = 1.3f;
-            zSFX.m_maxPitch = 1.5f;
-            zSFX.m_useCustomReverbDistance = true;
-            zSFX.m_customReverbDistance = 50f;
-            projectile.m_hitEffects.m_effectPrefabs[0].m_prefab = boom;
-            */
 
-            /*
-            Transform transformProj = ulty.transform;
-            transformProj.localScale = new Vector3(3.5f, 3.5f, 2f);
-            */
-
-            GameObject ulty = RRRLateLoadPrefabs.Clone(projectileName, "newFire", true, true);
             shared.m_attack.m_attackProjectile = ulty;
 
             return clone;
